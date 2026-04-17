7f2de24e2ef4d33d24e55176346a454633dd2c0f
Aman Tiwari <amantiwari@gmail.com>
2026-04-17 21:57:19 +0530

    class

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        Outer o=new Outer();
+        Outer.inner i=o.new inner();
+        i.display();
+
+
+    }
+}
+class Outer{
+    int x=10;
+    class inner{
+        void display(){
+            System.out.println(x);
+        }
+
+    }
+}
