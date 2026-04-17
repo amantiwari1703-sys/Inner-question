a1af8ad4b47f58d7c29812e69a919f5825af4e5c
Aman Tiwari <amantiwari@gmail.com>
2026-04-17 23:01:47 +0530

    nested

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Demo d = new Demo();
-        System.out.println(d.add(5, 3));
+        Outer o=new Outer();
+        o.callinner();
+
+        }
     }
-}
-abstract class Calculator{
-    abstract int add(int a,int b);
-}
-class Demo extends Calculator{
-    int add(int a,int b){
-        return a+b;
+    class Outer{
+    class inner{
+        void msg(){
+            System.out.println("hello inner");
+
+        }
+    }
+    void callinner(){
+        inner i=new inner();
+        i.msg();
+
+    }
     }
 
-}
+
+
 
